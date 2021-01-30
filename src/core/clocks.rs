feaa9df5087183e02c3a88abc45e18c4da731209
Matthew James Briggs <matthew.james.briggs@gmail.com>
2021-01-30 12:14:12 -0800

    document clocks

--- src/core/clocks.rs
+++ src/core/clocks.rs
@@ -1,16 +1,39 @@
-/// There are 24 MIDI Clocks in every quarter note. (12 MIDI Clocks in an eighth note, 6 MIDI Clocks in a 16th, etc).
+/// There are 24 MIDI Clocks in every quarter note. (12 MIDI Clocks in an eighth note, 6 MIDI Clocks
+/// in a 16th, etc). One example of using this enum is in the `TimeSignature`, where we can specify
+/// the frequency of the metronome click.
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd, Hash)]
 pub enum Clocks {
+    /// 142 MIDI clocks.
     DottedWhole,
+
+    /// 96 MIDI clocks.
     Whole,
+
+    /// 72 MIDI clocks.
     DottedHalf,
+
+    /// 48 MIDI clocks.
     Half,
+
+    /// 32 MIDI clocks.
     DottedQuarter,
+
+    /// 24 MIDI clocks.
     Quarter,
+
+    /// 18 MIDI clocks.
     DottedEighth,
+
+    /// 12 MIDI clocks.
     Eighth,
+
+    /// 9 MIDI clocks.
     DottedSixteenth,
+
+    /// 6 MIDI clocks.
     Sixteenth,
+
+    /// Any number of MIDI clocks, intended for durations not named above.
     Other(u8),
 }
 
@@ -21,6 +44,8 @@ impl Default for Clocks {
 }
 
 impl Clocks {
+    /// Create a new `Clocks` value from a `u8`, choosing one of the named variants if possible, and
+    /// falling back to `Other` if the value does not correspond to one of the named variants.
     pub(crate) fn from_u8(v: u8) -> Clocks {
         match v {
             142 => Clocks::DottedWhole,
@@ -37,6 +62,7 @@ impl Clocks {
         }
     }
 
+    // Get the `u8` value represented by the enum.
     pub(crate) fn to_u8(&self) -> u8 {
         match self {
             Clocks::DottedWhole => 142,
@@ -53,10 +79,20 @@ impl Clocks {
         }
     }
 
+    /// Create a new `Clocks` value from a `u8`, choosing one of the named variants if possible, and
+    /// falling back to `Other` if the value does not correspond to one of the named variants.
     pub fn new(clocks: u8) -> Self {
         Self::from_u8(clocks)
     }
 
+    /// If you create a `Clocks` value with a standard value, this will resolve the `Clocks` value
+    /// to a named variant instead of `Other`. For example:
+    /// ```
+    /// use midi_file::core::Clocks;
+    /// let mut clocks = Clocks::Other(24);
+    /// clocks.resolve();
+    /// assert!(matches!(clocks, Clocks::Quarter));
+    /// ```
     pub fn resolve(&mut self) {
         *self = Self::from_u8(self.to_u8())
     }
