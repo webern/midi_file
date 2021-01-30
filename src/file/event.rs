2b5b9dc4f6e2556c259c6090d435d95844f65109
Matthew James Briggs <matthew.james.briggs@gmail.com>
2021-01-30 12:03:47 -0800

    rename track_event.rs to track.rs

--- src/file/event.rs
+++ src/file/event.rs
@@ -9,56 +9,13 @@ use std::io::{Read, Write};
 
 /// `0xFF`: File Spec: All meta-events begin with FF, then have an event type byte (which is always
 /// less than 128)
-pub(crate) const FILE_META_EVENT: u8 = 0b1111_1111;
+const FILE_META_EVENT: u8 = 0b1111_1111;
 
 /// `0xF0`: File Spec: `F0 <length> <bytes to be transmitted after F0>`
-pub(crate) const FILE_SYSEX_F0: u8 = 0b1111_0000;
+const FILE_SYSEX_F0: u8 = 0b1111_0000;
 
 /// `0xF7`: File Spec: `F7 <length> <all bytes to be transmitted>`
-pub(crate) const FILE_SYSEX_F7: u8 = 0b1111_0111;
-
-/// <MTrk event> = <delta-time> <event>
-#[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd, Hash)]
-pub struct TrackEvent {
-    /// <delta-time> is stored as a variable-length quantity. It represents the amount of time
-    /// before the following event. If the first event in a track occurs at the very beginning of a
-    /// track, or if two events occur simultaneously, a delta-time of zero is used. Delta-times are
-    /// always present. Delta-time is in ticks as specified in the header chunk.
-    delta_time: u32,
-    event: Event,
-}
-
-impl TrackEvent {
-    pub fn new(delta_time: u32, event: Event) -> Self {
-        Self { delta_time, event }
-    }
-
-    pub fn delta_time(&self) -> u32 {
-        self.delta_time
-    }
-
-    pub fn event(&self) -> &Event {
-        &self.event
-    }
-
-    /// Returns true if the track event is a [`MetaEvent::EndOfTrack`].
-    pub(crate) fn is_end(&self) -> bool {
-        matches!(&self.event, Event::Meta(meta) if matches!(meta, MetaEvent::EndOfTrack))
-    }
-
-    pub(crate) fn parse<R: Read>(iter: &mut ByteIter<R>) -> LibResult<Self> {
-        let delta_time = iter.read_vlq_u32().context(io!())?;
-        trace!("delta_time {}", delta_time);
-        let event = Event::parse(iter)?;
-        Ok(Self { delta_time, event })
-    }
-
-    pub(crate) fn write<W: Write>(&self, w: &mut W) -> LibResult<()> {
-        let delta = Vlq::new(self.delta_time).to_bytes();
-        w.write_all(&delta).context(wr!())?;
-        self.event.write(w)
-    }
-}
+const FILE_SYSEX_F7: u8 = 0b1111_0111;
 
 /// <event> = <MIDI event> | <sysex event> | <meta-event>
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Hash)]
@@ -104,5 +61,48 @@ impl Event {
             Event::Sysex(sx) => sx.write(w),
             Event::Meta(mt) => mt.write(w),
         }
+    }
+}
+
+/// <MTrk event> = <delta-time> <event>
+#[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd, Hash)]
+pub struct TrackEvent {
+    /// <delta-time> is stored as a variable-length quantity. It represents the amount of time
+    /// before the following event. If the first event in a track occurs at the very beginning of a
+    /// track, or if two events occur simultaneously, a delta-time of zero is used. Delta-times are
+    /// always present. Delta-time is in ticks as specified in the header chunk.
+    delta_time: u32,
+    event: Event,
+}
+
+impl TrackEvent {
+    pub fn new(delta_time: u32, event: Event) -> Self {
+        Self { delta_time, event }
+    }
+
+    pub fn delta_time(&self) -> u32 {
+        self.delta_time
+    }
+
+    pub fn event(&self) -> &Event {
+        &self.event
+    }
+
+    /// Returns true if the track event is a [`MetaEvent::EndOfTrack`].
+    pub(crate) fn is_end(&self) -> bool {
+        matches!(&self.event, Event::Meta(meta) if matches!(meta, MetaEvent::EndOfTrack))
+    }
+
+    pub(crate) fn parse<R: Read>(iter: &mut ByteIter<R>) -> LibResult<Self> {
+        let delta_time = iter.read_vlq_u32().context(io!())?;
+        trace!("delta_time {}", delta_time);
+        let event = Event::parse(iter)?;
+        Ok(Self { delta_time, event })
+    }
+
+    pub(crate) fn write<W: Write>(&self, w: &mut W) -> LibResult<()> {
+        let delta = Vlq::new(self.delta_time).to_bytes();
+        w.write_all(&delta).context(wr!())?;
+        self.event.write(w)
     }
 }
--- src/file/mod.rs
+++ src/file/mod.rs
@@ -2,17 +2,17 @@
 //! These are separated from types and concepts that are also used in realtime MIDI (`core`).
 
 mod division;
+mod event;
 mod header;
 mod meta_event;
 mod sysex;
 mod track;
-mod track_event;
 
 pub use division::Division;
+pub use event::{Event, TrackEvent};
 pub use header::{Format, Header};
 pub use meta_event::{MetaEvent, MicrosecondsPerQuarter, QuartersPerMinute, TimeSignatureValue};
 pub use sysex::{SysexEvent, SysexEventType};
 pub use track::Track;
-pub use track_event::{Event, TrackEvent};
 
 pub(crate) use track::ensure_end_of_track;
