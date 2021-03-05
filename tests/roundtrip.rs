86e613361695cdd62f83d0bfd46c1bd0e04e8296
Matthew James Briggs <matthew.james.briggs@gmail.com>
2021-03-05 13:12:18 -0800

    running status test fixes
    - Make it possible to save and use an expected file for roundtrip tests.
    - Add expected files that fix two tests. The likely reason that these
      are needed is that the implementation that wrote the input files for
      these tests did not omit *all* possible running status bytes, but our
      implementation does. The files added are als_die_roemer.expected and
      tobeefree.expected.
    - Add a way to capture test output by setting an environment variable.
    - Enable the tests that had been skipped because we didn't understand
      running status.

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use midi_file::MidiFile;
 use std::fmt::{Debug, Display, Formatter};
+use std::path::{Path, PathBuf};
 use tempfile::TempDir;
 use utils::{
     enable_logging, test_file, ADESTE_FIDELES, ALS_DIE_ROEMER, AVE_MARIS_STELLA, BARITONE_SAX,
@@ -99,19 +100,20 @@ fn round_trip_test<S: AsRef<str>>(filename: S) -> RtResult {
     let in_path = test_file(&filename);
     let mf = MidiFile::load(&in_path).unwrap();
     mf.save(&out_path).unwrap();
+    capture_output(filename.as_ref(), &out_path);
 
-    let original_bytes = std::fs::read(&in_path).unwrap();
+    let expected_bytes = expected_bytes(&in_path);
     let saved_bytes = std::fs::read(&out_path).unwrap();
 
-    if original_bytes.len() != saved_bytes.len() {
+    if expected_bytes.len() != saved_bytes.len() {
         return Err(RtErr::Length(WrongLength {
             file: filename.as_ref().into(),
-            expected: original_bytes.len(),
+            expected: expected_bytes.len(),
             actual: saved_bytes.len(),
         }));
     }
 
-    for (ix, expected) in original_bytes.iter().enumerate() {
+    for (ix, expected) in expected_bytes.iter().enumerate() {
         let actual = saved_bytes[ix];
         if actual != *expected {
             rtfail!(filename, ix, *expected, actual);
@@ -123,6 +125,50 @@ fn round_trip_test<S: AsRef<str>>(filename: S) -> RtResult {
         return Err(RtErr::NotEqual(filename.as_ref().into()));
     }
     Ok(())
+}
+
+fn capture_dir() -> PathBuf {
+    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("output");
+    std::fs::create_dir_all(&dir).unwrap();
+    dir
+}
+
+fn capture_output<P: AsRef<Path>>(filename: &str, output: P) {
+    let val = match std::env::var("MIDI_FILE_CAPTURE_OUTPUT") {
+        Ok(s) => s,
+        Err(_) => return,
+    };
+    if val.as_str() != "true" {
+        return;
+    }
+    let save_path = capture_dir().join(filename);
+    let _ = std::fs::remove_file(&save_path);
+    let _ = std::fs::copy(output, &save_path).unwrap();
+}
+
+/// If there is a file with an `.expected` suffix, that file is read, otherwise the original file
+/// is read.
+fn expected_bytes<P: AsRef<Path>>(original_file: P) -> Vec<u8> {
+    let dir = original_file.as_ref().parent().unwrap();
+    let ext = original_file
+        .as_ref()
+        .extension()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let base_name = original_file
+        .as_ref()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let expected_name = base_name.replace(&format!(".{}", ext), ".expected");
+    let expected_path = dir.join(&expected_name);
+    if expected_path.exists() {
+        std::fs::read(&expected_path).unwrap()
+    } else {
+        std::fs::read(original_file.as_ref()).unwrap()
+    }
 }
 
 type BadFileTestResult = std::result::Result<(), BadFileTestError>;
@@ -160,9 +206,7 @@ fn adeste_fideles() {
     round_trip_test(ADESTE_FIDELES).unwrap();
 }
 
-// TODO - https://github.com/webern/midi/issues/1
 #[test]
-#[ignore]
 fn als_die_roemer() {
     round_trip_test(ALS_DIE_ROEMER).unwrap();
 }
@@ -192,16 +236,12 @@ fn logic_pro() {
     round_trip_test(LOGIC_PRO).unwrap();
 }
 
-// TODO - https://github.com/webern/midi/issues/1
 #[test]
-#[ignore]
 fn phobos_dorico() {
     round_trip_test(PHOBOS_DORICO).unwrap();
 }
 
-// TODO - https://github.com/webern/midi/issues/1
 #[test]
-#[ignore]
 fn tobeefree() {
     round_trip_test(TOBEFREE).unwrap();
 }
